6b645dd7083b56bd4101195dbde7837a632c09dd
miyagi <khalifapenuel21@gmail.com>
2018-10-26 21:56:31 +0300

    Form Validation

--- script.js
+++ script.js
@@ -470,4 +470,21 @@ Customer.prototype.toString = function(){
 };
 
 document.write(cust2.toString());
-
+// Form Validation 1:28:33
+function editNodeText(regex, input, helpId, helpMessage){
+    if(!regex.test(input)){
+        if(helpId != null)
+        while(helpId.childNodes[0]);{
+    }
+    helpId.appendChild(document.createTextNode(helpMessage));
+    } else {
+        if(helpId != null){
+            while(helpId.childNodes[0]){
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+        }
+    }
+}
+function isTheFieldEmpty(inputField, helpId){
+    return editNodeText(/^[A-Za-z\.\' \-]{1,15}\s?[A-Za-a\.\' \-] {1,15}\s?[A-Za-a\.\' \-] {1,15}/, inputField.value, helpId, "Please enter a valid name");
+}
