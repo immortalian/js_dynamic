dab2d750efeca973da455070cdab4fed367305bd
miyagi <khalifapenuel21@gmail.com>
2018-11-24 11:33:11 +0300

    Create Exception Handling

--- script.js
+++ script.js
@@ -488,3 +488,22 @@ function editNodeText(regex, input, helpId, helpMessage){
 function isTheFieldEmpty(inputField, helpId){
     return editNodeText(/^[A-Za-z\.\' \-]{1,15}\s?[A-Za-a\.\' \-] {1,15}\s?[A-Za-a\.\' \-] {1,15}/, inputField.value, helpId, "Please enter a valid name");
 }
+// Exception Handling 1:34:54
+var custArray = ["Tom", "Bob", "Sally", "Sue"];
+
+var getCust = function(index){
+    if(index > custArray.length){
+        throw new RangeError("Index must be >= 0 and <= " + custArray.length);
+        }else {
+            return custArray[index];
+        }
+}
+// try block
+try {
+    document.write("Customer : ", getCust(5), "<br />");
+}
+catch(ex){
+    if(ex instanceof RangeError){
+        document.write(ex.message + "<br />");
+    }
+}
